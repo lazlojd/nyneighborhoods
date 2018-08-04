734351b5d20d4c6a99050a6b53bc7a86fd5472ca
Leslie Jones <ljonesdove@uchicago.edu>
2018-08-04 18:19:47 -0400

    initial

---
+++ public/js/main.js
@@ -0,0 +1,82 @@
+(function () {
+	'use strict';
+
+	var map;
+    
+
+    function avgCoords(coords) {
+    	//console.log(coords);
+    	var latSum = 0, latCount = 0;
+    	var lngSum = 0, lngCount = 0;
+    	for (var data in coords) {
+    		latSum++;
+    		lngSum++;
+    		latCount += coords[data].lat;
+    		lngCount += coords[data].lng;
+    	}
+		return {lat: latCount/latSum, lng: lngCount/lngSum}
+    }
+
+    //Info window with name when location is tapped
+    var addListeners = function(polygon, name, avgCoords) {
+    	google.maps.event.addListener(polygon, 'click', function (event) {
+    		var infoWindow = new google.maps.InfoWindow({
+    			content: name,
+    			position: avgCoords
+    		})
+    		infoWindow.open(map)
+    	})
+    }
+	function drawNeighborhoods() {
+		var coordinates = boroughedNeighborhoods;
+    var hoods = Object.keys(boroughs);
+    for (var area in coordinates) {
+        // console.log(boroughs[area])
+        var borough = coordinates[area]
+        for(var neighborhood in borough) {
+           console.log(borough[neighborhood])
+           var data = borough[neighborhood];
+           // Find point to in neighborhood to assign info window to
+           var avgC = avgCoords(data.coords);
+           var border = new google.maps.Polygon({
+              path: data.coords,
+              geodesic: true,
+              strokeColor: '#' + data.color,
+              strokeOpacity: 1.0,
+              strokeWeight: 2,
+              fillColor: '#' + data.color,
+              fillOpacity: 0.25
+            });
+            addListeners(border, neighborhood, avgC)
+            border.setMap(map)
+        }
+      }
+		
+      
+	}
+
+    function initMap(x, y) {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: x, lng: y},
+          zoom: 13
+        });
+        var marker = new google.maps.Marker({position: {lat: x, lng: y}, map: map});
+        //console.log(boroughedNeighborhoods.manhattan.)
+  		drawNeighborhoods()
+     }
+    function onPositionRecieved(position){
+    	var coords = position.coords;
+    	initMap(coords.latitude, coords.longitude);
+    }
+
+    function locationNotRecieved(positionError){
+    	console.log(positionError);
+    }
+    if(navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(onPositionRecieved, locationNotRecieved);
+    }
+
+
+} ());
+
+
